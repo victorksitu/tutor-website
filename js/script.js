d5b0c673b3e7cd48c52846546acce139479f8693
Victor Situ <vsitu011@uottawa.ca>
2024-10-17 19:12:57 -0400

    finished home and chapters

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Toggle mobile menu
+document.addEventListener('DOMContentLoaded', function() {{
+    const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
+
+    toggleButton.addEventListener('click', function() {
+        mobileMenu.classList.toggle('active');
+    })
+
+    mobileMenu.addEventListener('click', function() {
+        mobileMenu.classList.remove('active');
+    })
+}})
+
+// Change navbar background on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    
+    if (this.window.scrollY > 0) {
+        navbar.classList.add('navbar-scroll');
+    } else {
+        navbar.classList.remove('navbar-scroll');
+    }
+})
